efd31e7d39f395f488499e9fdc21138bdd6de8c2
fabiandonaque <fabian.donaque@fabsrobotics.com>
2023-05-25 21:14:59 +0200

    Second video

--- server/main.go
+++ server/main.go
@@ -3,18 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 )
 
-func MainRouter(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello World!"))
+func ServeFile(w http.ResponseWriter, r *http.Request, path string) {
+	safePath, err := url.QueryUnescape(path)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	http.ServeFile(w, r, safePath)
 }
 
-func main(){
-	fmt.Println("Server initialized on port "+os.Getenv("PORT"))
+func GetRouter(w http.ResponseWriter, r *http.Request) {
+	ServeFile(w, r, "static/"+r.URL.Path)
+}
+
+func MainRouter(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		GetRouter(w, r)
+	} else {
+		w.Write([]byte("Method not permited"))
+	}
+}
+
+func main() {
+	fmt.Println("Server initialized on port " + os.Getenv("PORT"))
 	// Main Router
-	http.HandleFunc("/",MainRouter)
+	http.HandleFunc("/", MainRouter)
 	// Listen And Serve
-	err := http.ListenAndServe(":"+os.Getenv("PORT"),nil)
-	if err != nil { panic(err) }
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
