e1dc9ac39e6f39aa78e0db76dcf6507568d97ea0
fabiandonaque <fabian.donaque@fabsrobotics.com>
2023-07-29 08:23:43 +0200

    resolved model package cycle

--- server/models/nature/nature.go
+++ server/models/nature/nature.go
@@ -7,33 +7,35 @@ import (
 
 func CreateOrUpdateTable(db *mariadb.Connection) error {
 	// Create table nature
-	_,err := db.Create("CREATE TABLE IF NOT EXISTS `accounting_nature` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(45) NOT NULL, `increment` TINYINT NOT NULL, PRIMARY KEY(`id`) )")
+	_, err := db.Create("CREATE TABLE IF NOT EXISTS `accounting_nature` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(45) NOT NULL, `increment` TINYINT NOT NULL, PRIMARY KEY(`id`) )")
 	return err
 }
 
-func New(db *mariadb.Connection,name string, increment bool) error {
+func New(db *mariadb.Connection, name string, increment bool) error {
 	// Insert new nature
-	_,err := db.Insert("INSERT INTO `accounting_nature` (`name`,`increment`) VALUES (?,?)",name,increment)
+	_, err := db.Insert("INSERT INTO `accounting_nature` (`name`,`increment`) VALUES (?,?)", name, increment)
 	return err
 }
 
-func UpdateName(db *mariadb.Connection,id int64, newName string) error {
+func UpdateName(db *mariadb.Connection, id int64, newName string) error {
 	// Update nature name
-	_,err := db.Update("UPDATE `accounting_nature` SET `name` = ? WHERE `id` = ?",newName,id)
+	_, err := db.Update("UPDATE `accounting_nature` SET `name` = ? WHERE `id` = ?", newName, id)
 	return err
 }
 
-func UpdateIncrement(db *mariadb.Connection,id int64, newIncrement bool) error {
+func UpdateIncrement(db *mariadb.Connection, id int64, newIncrement bool) error {
 	// Update nature increment
-	_,err := db.Update("UPDATE `accounting_nature` SET `increment` = ? WHERE `id` = ?",newIncrement,id)
+	_, err := db.Update("UPDATE `accounting_nature` SET `increment` = ? WHERE `id` = ?", newIncrement, id)
 	return err
 }
 
-func Delete(db *mariadb.Connection,id int64) error {
+func Delete(db *mariadb.Connection, id int64) error {
 	// Delete all accounts with this nature
-	err := account.DeleteAllByNatureId(id)
-	if err != nil { return err }
+	err := account.DeleteAllByNatureId(db, id)
+	if err != nil {
+		return err
+	}
 	// Delete nature
-	_,err = db.Delete("DELETE FROM `accounting_nature` WHERE `id` = ?",id)
+	_, err = db.Delete("DELETE FROM `accounting_nature` WHERE `id` = ?", id)
 	return err
 }
--- server/models/taxyear/taxyear.go
+++ server/models/taxyear/taxyear.go
@@ -7,54 +7,61 @@ import (
 	"regexp"
 )
 
-func CreateOrUpdateTable(db *mariadb.Connection){
+func CreateOrUpdateTable(db *mariadb.Connection) {
 	// Create table taxyear
-	_,err := db.Create("CREATE TABLE IF NOT EXISTS `accounting_tax_year` (`year` INT NOT NULL, `init_date` DATE NOT NULL, `end_date` DATE NOT NULL, PRIMARY KEY(`year`) )")
-	if err != nil { return }
+	_, err := db.Create("CREATE TABLE IF NOT EXISTS `accounting_tax_year` (`year` INT NOT NULL, `init_date` DATE NOT NULL, `end_date` DATE NOT NULL, PRIMARY KEY(`year`) )")
+	if err != nil {
+		return
+	}
 }
 
 func New(db *mariadb.Connection, year int64, startDate string, endDate string) error {
 	re := regexp.MustCompile(`\d{4}-\d{2}-\d{2}`)
 	// Check startDate follows format
-	if !re.MatchString(startDate){ return fmt.Errorf("startDate doesn't follow format YYYY-MM-DD")}
+	if !re.MatchString(startDate) {
+		return fmt.Errorf("startDate doesn't follow format YYYY-MM-DD")
+	}
 	// Check endDate follows format
-	if !re.MatchString(startDate){ return fmt.Errorf("endDate doesn't follow format YYYY-MM-DD")}
+	if !re.MatchString(startDate) {
+		return fmt.Errorf("endDate doesn't follow format YYYY-MM-DD")
+	}
 	// Check Year doesn't already exists
-	raw,err := db.Select("SELECT * FROM `taxyear` WHERE `year` = ?",year)
-	if err != nil { return err }
-	if len(raw) > 2 { return fmt.Errorf("year already exists") }
+	raw, err := db.Select("SELECT * FROM `taxyear` WHERE `year` = ?", year)
+	if err != nil {
+		return err
+	}
+	if len(raw) > 2 {
+		return fmt.Errorf("year already exists")
+	}
 	return nil
 }
 
 func UpdateYear(db *mariadb.Connection, oldYear int64, newYear int64) error {
 	// Check new year doesn't already exists
-	raw,err := db.Select("SELECT * FROM `accounting_tax_year` WHERE `year` = ?",newYear)
-	if err != nil { return err }
-	if len(raw) > 2 { return fmt.Errorf("year already exists") }
+	raw, err := db.Select("SELECT * FROM `accounting_tax_year` WHERE `year` = ?", newYear)
+	if err != nil {
+		return err
+	}
+	if len(raw) > 2 {
+		return fmt.Errorf("year already exists")
+	}
 	// Update year
-	_,err = db.Update("UPDATE `accounting_tax_year` SET `year` = ? WHERE `year` = ?",newYear,oldYear)
-	if err != nil { return err }
+	_, err = db.Update("UPDATE `accounting_tax_year` SET `year` = ? WHERE `year` = ?", newYear, oldYear)
+	if err != nil {
+		return err
+	}
 	// Update all account movements year
-	err = movement.ChangeYearOfAll(db,oldYear,newYear)
+	err = movement.ChangeYearOfAll(db, oldYear, newYear)
 	return err
 }
 
 func Delete(db *mariadb.Connection, year int64) error {
 	// Delete all movements from this year
-	err := movement.DeleteAllByYear(db,year)
-	if err != nil { return err }
-	// Delete year
-	_,err = db.Delete("DELETE FROM `accounting_tax_year` WHERE `year` = ?",year)
-	return err
-}
-
-func CheckYearExistence(db *mariadb.Connection, year int64) (bool,error) {
-	// Check if year already exists
-	raw,err := db.Select("SELECT * FROM `accounting_tax_year` WHERE `year` = ?",year)
-	if err != nil { return false,err }
-	if len(raw) > 2 {
-		return true,nil
-	} else {
-		return false,nil
+	err := movement.DeleteAllByYear(db, year)
+	if err != nil {
+		return err
 	}
+	// Delete year
+	_, err = db.Delete("DELETE FROM `accounting_tax_year` WHERE `year` = ?", year)
+	return err
 }
